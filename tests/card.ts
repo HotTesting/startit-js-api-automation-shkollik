c586c8b6855f06baff4dd3fe0a8c2f5b4f95f12f
Vladyslav Shkola <Vladyslav_Shkola@epam.com>
2019-04-23 09:52:34 -0500

    added json-schema package and renamed constants

--- tests/card.ts
+++ tests/card.ts
@@ -5,12 +5,16 @@ import {SwimlaneController} from "../framework2/service/controllers/swimlane_con
 import {ListController} from "../framework2/service/controllers/list_controller"
 import {CardController} from "../framework2/service/controllers/card_controller"
 import * as faker from "faker"
-import {expect} from "chai"
+import * as chai  from "chai"
+chai.use(require("chai-json-schema-ajv"));
+const expect = chai.expect;
 
-const domain:string = "ip-5236.sunline.net.ua";
-const origin:string = "http://ip-5236.sunline.net.ua:30020";
-const rootEmail:string = "test@test.com";
-const rootPassword:string = "123456";
+
+const DOMAIN:string = "ip-5236.sunline.net.ua";
+const ORIGIN:string = "http://ip-5236.sunline.net.ua:30020";
+const ROOT_EMAIL:string = "test@test.com";
+const ROOT_PASSWORD:string = "123456";
+const CARD_ID_LENGTH = 17
 let rootToken:string;
 let testUserName:string;
 let testUserPassword:string;
@@ -23,22 +27,22 @@ let testCardId:string;
 
 
 describe("Card API", function(){
-    beforeEach("create a test user, board, list", async function(){
+    beforeEach("create a test user, board, swimlane, list", async function(){
         //get token
         let userController = new UserController();
-        let adminLoginResponse = await userController.login(rootEmail, rootPassword);
+        let adminLoginResponse = await userController.login(ROOT_EMAIL, ROOT_PASSWORD);
         rootToken = adminLoginResponse.token;
         
         //create user
         testUserName = faker.internet.userName();
         testUserPassword = faker.internet.password(11);
-        testUserEmail = faker.internet.email(undefined, undefined, domain); 
-        let adminUserController = new UserController(origin, undefined, rootToken);
+        testUserEmail = faker.internet.email(undefined, undefined, DOMAIN); 
+        let adminUserController = new UserController(ORIGIN, undefined, rootToken);
         let createUserResponse = await adminUserController.createUser(testUserEmail, testUserPassword, testUserName);
         testUserId = createUserResponse._id;   
         
         //create board
-        let boardController = new BoardController(origin, undefined, rootToken);
+        let boardController = new BoardController(ORIGIN, undefined, rootToken);
         let testBoardName = faker.random.word.toString();
         let testBoardPermission = "private";
         let testBoardColor = "nephritis";
@@ -47,46 +51,50 @@ describe("Card API", function(){
         
         //create swimlane
         let testSwimlaneTitle = faker.random.word.toString();
-        let swimlaneController = new SwimlaneController(origin, undefined, rootToken);
+        let swimlaneController = new SwimlaneController(ORIGIN, undefined, rootToken);
         let createSwimlaneResponse = await swimlaneController.createSwimlane(testBoardId, testSwimlaneTitle);
         testSwimlaneId = createSwimlaneResponse._id;
 
         //create list
         let testListTitle = faker.random.word.toString();
-        let listController = new ListController(origin, undefined, rootToken);
+        let listController = new ListController(ORIGIN, undefined, rootToken);
         let createListResponse = await listController.createList(testBoardId, testListTitle);
         testListId = createListResponse._id;
     });
 
     afterEach("delete a test user, board, list", async function(){
-        let adminUserController = new UserController(origin, undefined, rootToken);
+        let adminUserController = new UserController(ORIGIN, undefined, rootToken);
         let deleteUserResponse = await adminUserController.deleteUser(testUserId);
 
-        let boardController = new BoardController(origin, undefined, rootToken);
+        let boardController = new BoardController(ORIGIN, undefined, rootToken);
         let deleteBoardResponse = await boardController.deleteBoard(testBoardId);
 
-        let swimlaneController = new SwimlaneController(origin, undefined, rootToken);
+        let swimlaneController = new SwimlaneController(ORIGIN, undefined, rootToken);
         let deleteSwimlaneResponse = await swimlaneController.deleteSwimlane(testBoardId, testSwimlaneId);
 
-        let listController = new ListController(origin, undefined, rootToken);
+        let listController = new ListController(ORIGIN, undefined, rootToken);
         let deleteListResponse = await listController.deleteList(testBoardId, testListId);
 
-        let cardController = new CardController(origin, undefined, rootToken);
+        let cardController = new CardController(ORIGIN, undefined, rootToken);
         let deleteCardResponse = await cardController.deleteCard(testBoardId, testListId, testCardId);
     });
 
     it("Should create a card", async function(){
-        //create list
         let testCardTitle = faker.random.word.toString();
         let testCardDescrition = faker.random.word.toString();
-        let cardController = new CardController(origin, undefined, rootToken);
+        let cardController = new CardController(ORIGIN, undefined, rootToken);
         let createCardResponse = await cardController.createCard(testBoardId, testListId, testCardTitle, testCardDescrition, testUserId, testSwimlaneId);
         testCardId = createCardResponse._id;
 
-        expect(createCardResponse, JSON.stringify(createCardResponse))
-            .to.be.an("object")
-            .that.has.key("_id");
-     });
-
-     
+        expect(createCardResponse).to.be.jsonSchema({
+            type: "object",
+            properties:{
+                _id:{
+                    type:"string",
+                    minLength: CARD_ID_LENGTH,
+                    maxLength: CARD_ID_LENGTH
+                }
+            }            
+        });
+     });     
 });
